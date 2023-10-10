cd66cda3c19a4d10c0aa5c49d7f78b12b0430dd4
Riosmena <emi.riosmena@gmail.com>
2023-10-10 15:20:18 -0600

    Add images

--- Peliculas/app/src/main/java/com/example/kotlin/peliculas/PeliculaViewHolder.kt
+++ Peliculas/app/src/main/java/com/example/kotlin/peliculas/PeliculaViewHolder.kt
@@ -2,9 +2,13 @@ package com.example.kotlin.peliculas
 
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin.peliculas.databinding.ItemPeliculaBinding
+import com.squareup.picasso.Picasso
 
 class PeliculaViewHolder(private val binding: ItemPeliculaBinding) : RecyclerView.ViewHolder(binding.root) {
     fun bind(item: PeliculaBase){
         binding.TVTitle.text = item.title
+
+        val imageUrl = Constants.BASE_IMG_URL + item.poster_path
+        Picasso.get().load(imageUrl).into(binding.IVPhoto)
     }
 }
